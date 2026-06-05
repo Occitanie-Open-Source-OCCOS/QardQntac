6a0416114991104df86d9dc5796611d1066d55a8
Théo LAGACHE <theo.lagache@soluce-technologies.com>
2026-06-05 16:27:36 +0200

    fix

--- config/next.ts
+++ config/next.ts
@@ -5,49 +5,49 @@ import { buildCSPHeader, buildPermissionsPolicy } from "./security";
 const withNextIntl = createNextIntlPlugin("./src/lib/i18n.ts");
 
 export const nextConfig: NextConfig = withNextIntl({
-	output: "standalone",
-	images: {},
-	cacheComponents: false,
-	experimental: {
-		serverActions: {
-			bodySizeLimit: "8mb",
-		},
-	},
-	logging: {
-		browserToTerminal: false,
-	},
-	allowedDevOrigins: ["192.168.1.213"],
-	async headers() {
-		return [
-			{
-				source: "/(.*)",
-				headers: [
-					{
-						key: "Content-Security-Policy",
-						value: buildCSPHeader(),
-					},
-					{
-						key: "Permissions-Policy",
-						value: buildPermissionsPolicy(),
-					},
-					{
-						key: "X-Content-Type-Options",
-						value: "nosniff",
-					},
-					{
-						key: "X-Frame-Options",
-						value: "DENY",
-					},
-					{
-						key: "Referrer-Policy",
-						value: "strict-origin-when-cross-origin",
-					},
-					{
-						key: "Strict-Transport-Security",
-						value: "max-age=63072000; includeSubDomains; preload",
-					},
-				],
-			},
-		];
-	},
+  output: "standalone",
+  images: {},
+  cacheComponents: false,
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "8mb",
+    },
+  },
+  logging: {
+    browserToTerminal: false,
+  },
+  allowedDevOrigins: ["192.168.1.213", "192.168.1.204"],
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: buildCSPHeader(),
+          },
+          {
+            key: "Permissions-Policy",
+            value: buildPermissionsPolicy(),
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "X-Frame-Options",
+            value: "DENY",
+          },
+          {
+            key: "Referrer-Policy",
+            value: "strict-origin-when-cross-origin",
+          },
+          {
+            key: "Strict-Transport-Security",
+            value: "max-age=63072000; includeSubDomains; preload",
+          },
+        ],
+      },
+    ];
+  },
 });
