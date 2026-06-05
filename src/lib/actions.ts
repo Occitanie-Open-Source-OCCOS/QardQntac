072f6b76232ef05ff5000dc9700cff04b579a024
Théo LAGACHE <theo.lagache@soluce-technologies.com>
2026-06-05 14:44:34 +0200

    fix

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -3,44 +3,45 @@ import { createSafeActionClient } from "next-safe-action";
 import authServer from "./auth/server";
 
 export const action = createSafeActionClient({
-	handleServerError(e) {
-		if (e instanceof Error) {
-			return e.message;
-		}
-		return "An unknown error occurred";
-	},
+  handleServerError(e) {
+    if (e instanceof Error) {
+      return e.message;
+    }
+    return "An unknown error occurred";
+  },
 });
 
 export const authentificatedAction = action.use(async ({ next }) => {
-	const session = await authServer.api.getSession({
-		headers: await headers(),
-	});
+  const session = await authServer.api.getSession({
+    headers: await headers(),
+  });
 
-	if (!session || session.user.role !== "user") {
-		throw new Error("Unauthorized.");
-	}
+  //@ts-ignore
+  if (!session || session.user.role !== "user") {
+    throw new Error("Unauthorized.");
+  }
 
-	return next({
-		ctx: {
-			userId: session.user.id,
-			user: session.user,
-		},
-	});
+  return next({
+    ctx: {
+      userId: session.user.id,
+      user: session.user,
+    },
+  });
 });
 
 export const anyAuthenticatedAction = action.use(async ({ next }) => {
-	const session = await authServer.api.getSession({
-		headers: await headers(),
-	});
+  const session = await authServer.api.getSession({
+    headers: await headers(),
+  });
 
-	if (!session) {
-		throw new Error("Unauthorized.");
-	}
+  if (!session) {
+    throw new Error("Unauthorized.");
+  }
 
-	return next({
-		ctx: {
-			userId: session.user.id,
-			user: session.user,
-		},
-	});
+  return next({
+    ctx: {
+      userId: session.user.id,
+      user: session.user,
+    },
+  });
 });
